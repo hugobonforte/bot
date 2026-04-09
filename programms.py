e464f96324f91e84ea020f5d722b99812623d973
hugobonforte <hugobonforte@gmail.com>
2026-04-09 14:03:18 -0600

    Implement Discord bot with password generation
    Added a Discord bot that responds to commands for generating passwords and greetings.

--- programms.py
+++ programms.py
@@ -1,1 +1,29 @@
+import discord
 
+from bot_logic import gen_pass 
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'--- Botinsano encendido con prefijo "+" como {client.user} ---')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content.lower()
+
+    if msg.startswith('+pass'):
+ 
+        password_generada = gen_pass(10)
+        await message.channel.send(f"🔑 Tu contraseña segura es: `{password_generada}`")
+
+ 
+    elif msg.startswith('+hello'):
+        await message.channel.send("¡Qué onda! Ahora respondo a los comandos con el signo + 😎")
+      
+client.run("MTQ4Njg5ODUwNjk5MDg3ODc2MA.GZul4u.qxpu3JWcqVpiqFOH0zhtk3g4Fo4VepqjNvyk4U")
