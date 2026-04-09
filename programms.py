a9cf2226d210fa1c3e808294eddaabade67c1151
hugobonforte <hugobonforte@gmail.com>
2026-04-09 14:10:35 -0600

    Change command prefix from '+' to '¡'

--- programms.py
+++ programms.py
@@ -8,7 +8,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'--- Botinsano encendido con prefijo "+" como {client.user} ---')
+    print(f'- Botinsano encendido con prefijo "¡" como {client.user} -')
 
 @client.event
 async def on_message(message):
@@ -17,13 +17,13 @@ async def on_message(message):
 
     msg = message.content.lower()
 
-    if msg.startswith('+pass'):
+    if msg.startswith('¡pass'):
  
         password_generada = gen_pass(10)
         await message.channel.send(f"🔑 Tu contraseña segura es: `{password_generada}`")
 
  
-    elif msg.startswith('+hello'):
-        await message.channel.send("¡Qué onda! Ahora respondo a los comandos con el signo + 😎")
+    elif msg.startswith('¡hello'):
+        await message.channel.send("¡Qué onda! Ahora respondo a los comandos con el signo ¡ 😎")
       
 client.run("MTQ4Njg5ODUwNjk5MDg3ODc2MA.GZul4u.qxpu3JWcqVpiqFOH0zhtk3g4Fo4VepqjNvyk4U")
